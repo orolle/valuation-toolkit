ed725f1588422b7e8cd3a6096aaecf4a3233ef48
orolle <oliver.rolle@googlemail.com>
2012-12-13 04:03:27 +0100

    Update valuationtoolkit.js

--- valuationtoolkit.js
+++ valuationtoolkit.js
@@ -1,2 +1,15 @@
-
-alert('Hello Value Investor!');
+var ValuationTK = (function () {
+  var my = {},
+		privateVariable = 1;
+	
+	function privateMethod() {
+		// ...
+	}
+	
+	my.moduleProperty = 1;
+	my.hello = function () {
+	  alert('Hello Investor!');
+	};
+	
+	return my;
+}());
